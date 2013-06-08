8da2cdef409e5b742e85f21afba2e9b880b04ba9
Benjamin Gehrels <github@gehrels>
2013-06-08 12:37:52 +0200

    shorter name for generic CANDIDATE_TYPE -> CANDIDATE

--- src/main/java/info/gehrels/voting/singleTransferableVote/VotesByCandidateCalculation.java
+++ src/main/java/info/gehrels/voting/singleTransferableVote/VotesByCandidateCalculation.java
@@ -4,18 +4,17 @@ import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.ImmutableSet;
-import info.gehrels.voting.Candidate;
 import org.apache.commons.math3.fraction.BigFraction;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public final class VotesByCandidateCalculation {
-	static <CANDIDATE_TYPE extends Candidate> Map<CANDIDATE_TYPE, BigFraction> calculateVotesByCandidate(ImmutableSet<CANDIDATE_TYPE> candidates,
-	                                                             ImmutableCollection<BallotState<CANDIDATE_TYPE>> ballotStates) {
-		Map<CANDIDATE_TYPE, BigFraction> votesByCandidateDraft = new HashMap<>();
-		for (BallotState<CANDIDATE_TYPE> ballotState : ballotStates) {
-			CANDIDATE_TYPE preferredHopefulCandidate = ballotState.getPreferredCandidate();
+	static <CANDIDATE extends info.gehrels.voting.Candidate> Map<CANDIDATE, BigFraction> calculateVotesByCandidate(ImmutableSet<CANDIDATE> candidates,
+	                                                             ImmutableCollection<BallotState<CANDIDATE>> ballotStates) {
+		Map<CANDIDATE, BigFraction> votesByCandidateDraft = new HashMap<>();
+		for (BallotState<CANDIDATE> ballotState : ballotStates) {
+			CANDIDATE preferredHopefulCandidate = ballotState.getPreferredCandidate();
 			if (preferredHopefulCandidate == null) {
 				continue;
 			}
@@ -28,8 +27,8 @@ public final class VotesByCandidateCalculation {
 			}
 		}
 
-		Builder<CANDIDATE_TYPE, BigFraction> builder = ImmutableMap.builder();
-		for (CANDIDATE_TYPE candidate : candidates) {
+		Builder<CANDIDATE, BigFraction> builder = ImmutableMap.builder();
+		for (CANDIDATE candidate : candidates) {
 			BigFraction votes = votesByCandidateDraft.get(candidate);
 			builder.put(candidate, votes == null ? BigFraction.ZERO : votes);
 		}
