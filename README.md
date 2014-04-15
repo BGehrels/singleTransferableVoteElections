3e509e0b1f102d1304707c62b427d6a90b29f4b4
Benjamin Gehrels <github@gehrels>
2014-04-15 02:59:30 +0200

    Lincensing the library under GNU AGPL v3 or later

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Single Transferable Vote Elections
 ==================================
 
-A library to calculate results of Single Transferable Vote elections using the Weighted Inclusive Gregory Method
+A library to calculate results of Single Transferable Vote elections using the Weighted Inclusive Gregory Method.
--- src/test/java/info/gehrels/voting/singleTransferableVote/CandidateStatesTest.java
+++ src/test/java/info/gehrels/voting/singleTransferableVote/CandidateStatesTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2014 Benjamin Gehrels
+ *
+ * This file is part of The Single Transferable Vote Elections Library.
+ *
+ * The Single Transferable Vote Elections Web Interface is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * The Single Transferable Vote Elections Web Interface is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with The Single Transferable Vote
+ * Elections Web Interface. If not, see <http://www.gnu.org/licenses/>.
+ */
 package info.gehrels.voting.singleTransferableVote;
 
 import com.google.common.collect.ImmutableSet;
