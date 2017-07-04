388ce2b1fcbb40432dc46e8f07918391ea042237
Benjamin Gehrels <github@gehrels>
2017-07-04 13:11:38 +0200

    Added unit tests for the Ballot-to-new-Election-instance migration

--- src/test/java/info/gehrels/voting/BallotTest.java
+++ src/test/java/info/gehrels/voting/BallotTest.java
@@ -23,14 +23,21 @@ import static info.gehrels.voting.OptionalMatchers.anAbsentOptional;
 import static info.gehrels.voting.Vote.createPreferenceVote;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.sameInstance;
 
 public final class BallotTest {
-	private static final Candidate CANDIDATE = new Candidate("Peter");
+	private static final Candidate CANDIDATE_1 = new Candidate("Peter");
+	private static final Candidate CANDIDATE_2 = new Candidate("Peter");
 
-	private static final Election<Candidate> ELECTION_1 = new Election<>("Office1", ImmutableSet.of(CANDIDATE));
-	private static final Election<Candidate> ELECTION_2 = new Election<>("Office2", ImmutableSet.of(CANDIDATE));
+	private static final Election<Candidate> ELECTION_1 = new Election<>("Office1", ImmutableSet.of(CANDIDATE_1));
+	private static final Election<Candidate> NEW_ELECTION_1 = new Election<>("Office 1 migrated", ImmutableSet.of(CANDIDATE_1));
+	private static final Election<Candidate> ELECTION_2 = new Election<>("Office2", ImmutableSet.of(CANDIDATE_2));
+
 	private static final Vote<Candidate> VOTE_FOR_ELECTION_1 = createPreferenceVote(ELECTION_1,
-	                                                                               ImmutableSet.of(CANDIDATE));
+	                                                                               ImmutableSet.of(CANDIDATE_1));
+	private static final Vote<Candidate> VOTE_FOR_ELECTION_2 = createPreferenceVote(ELECTION_2,
+	                                                                               ImmutableSet.of(CANDIDATE_2));
 
 	@Test
 	public void returnsAbsentOptionalIfBallotContainsNoVoteForThisElection() {
@@ -41,12 +48,37 @@ public final class BallotTest {
 		assertThat(ballot.getVote(ELECTION_2), is(anAbsentOptional()));
 	}
 
-
 	@Test
 	public void returnsTheVoteIfBallotContainsOneThisElection() {
 		ImmutableSet<Vote<Candidate>> votes = ImmutableSet.of(VOTE_FOR_ELECTION_1);
 		Ballot<Candidate> ballot = new Ballot<>(0, votes);
 
 		assertThat(ballot.getVote(ELECTION_1).get().getElection(), is(ELECTION_1));
+	}
+
+	@Test
+	public void withReplacedElectionReturnsNewBallotWithMigratedVotes() {
+		ImmutableSet<Vote<Candidate>> votes = ImmutableSet.of(VOTE_FOR_ELECTION_1);
+		Ballot<Candidate> originalBallot = new Ballot<>(0, votes);
+
+		Ballot<Candidate> newBallot = originalBallot.withReplacedElection(ELECTION_1.getOfficeName(), NEW_ELECTION_1);
+
+		assertThat(newBallot, is(not(sameInstance(originalBallot))));
+		assertThat(newBallot.getVote(ELECTION_1).isPresent(), is(false));
+		assertThat(newBallot.getVote(NEW_ELECTION_1).isPresent(), is(true));
+		assertThat(newBallot.getVote(NEW_ELECTION_1).get().getElection(), is(NEW_ELECTION_1));
+		assertThat(newBallot.getVote(NEW_ELECTION_1).get().getRankedCandidates().iterator().next(), is(sameInstance(CANDIDATE_1)));
+	}
+
+	@Test
+	public void withReplacedElectionWontTouchUnrelatedElectionVotes() {
+		ImmutableSet<Vote<Candidate>> votes = ImmutableSet.of(VOTE_FOR_ELECTION_1, VOTE_FOR_ELECTION_2);
+		Ballot<Candidate> originalBallot = new Ballot<>(0, votes);
+
+		Ballot<Candidate> newBallot = originalBallot.withReplacedElection(ELECTION_1.getOfficeName(), NEW_ELECTION_1);
+
+		assertThat(newBallot, is(not(sameInstance(originalBallot))));
+		assertThat(newBallot.getVote(ELECTION_2).isPresent(), is(true));
+		assertThat(newBallot.getVote(ELECTION_2).get(), is(sameInstance(VOTE_FOR_ELECTION_2)));
 	}
 }
